e5b4fd8fbf8f9b1ded5a5f067e1543ce0c60bdf0
j4v1N <javiinavarreteg@gmail.com>
2021-07-09 04:42:21 -0400

    semana 2
    archivos comic

---
+++ commlab/semana2/js/script.js
@@ -0,0 +1,8 @@
+alert("Hola! acompañame a ver esta história");
+function CambiaContenidoS() {
+  document.getElementById('1') .src ="imágenes/Cómic.sacarlo.jpg";
+}
+function CambiarContenidoI() {
+  document.getElementById('1'). src= "imágenes/Cómic.ignorarlo.jpg";
+}
+// document.getElementById('Boom').src="audio/Efecto de sonido de explosión.ogg" ;
