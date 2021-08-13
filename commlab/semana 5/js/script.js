15470a15eac88d4b0f040d16938d5a28b8551b72
j4v1N <javiinavarreteg@gmail.com>
2021-08-13 03:14:58 -0400

    semana 5
    Proyecto Taller

---
+++ commlab/semana 5/js/script.js
@@ -0,0 +1,11 @@
+function changeImage() {
+
+     if (document.getElementById("photo").src == "imagenes/corte l.jpg" )
+     {
+         // document.getElementById("photo").src = "imagenes/cortev.jpg";
+     }
+     else
+     {
+         document.getElementById("photo").src = "imagenes/corte v.jpg";
+     }
+ }
