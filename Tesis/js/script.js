2db0b10acbcc24ba23fa623368665719eba12507
j4v1N <javiinavarreteg@gmail.com>
2023-12-17 04:00:29 -0300

    Tesis. La iluminación como forma espacial.
    Creación de pagina web de tesis

---
+++ Tesis/js/script.js
@@ -0,0 +1,162 @@
+function AbreIntro1(){
+  var x = document.getElementById('intro1');
+  if(x.style.display==='none'){
+    x.style.display = 'inline-grid';
+    x.style.placeitems = 'center' ;
+    x.style.marginleft = 'auto';
+    x.style.marginright = 'auto' ;
+  } else{
+    x.style.display = 'none';
+  }
+  var y = document.getElementById('menu');
+  if(y.style.visibility==='hidden'){
+    y.style.visibility = 'visible';
+  } else {
+    y.style.visibility = 'hidden';
+  }
+}
+
+function Cerrar() {
+  var x= document.getElementById('intro1');
+  if(x.style.display==='inline-grid'){
+    x.style.display = 'none';
+  }
+  var y = document.getElementById('menu');
+  if(y.style.visibility=== 'visible'){
+    y.style.visibility = 'hidden';
+  }
+  var z= document.getElementById('elem1');
+  if(z.style.display==='inline-grid'){
+    z.style.display = 'none';
+  }
+  var w = document.getElementById('menu1');
+  if(w.style.visibility==='visible'){
+    w.style.visibility ='hidden';
+  }
+  var v= document.getElementById('ana1');
+  if(v.style.display==='inline-grid'){
+    v.style.display = 'none';
+  }
+  var u =document.getElementById('menu2');
+  if(u.style.visibility==='visible'){
+    u.style.visibility = 'hidden';
+  }
+  var t= document.getElementById('pen1');
+  if(t.style.display==='inline-grid'){
+    t.style.display = 'none';
+  }
+  var s= document.getElementById('menu3');
+  if(s.style.visibility==='visible'){
+    s.style.visibility = 'hidden';
+  }
+  var r= document.getElementById('luz1');
+  if(r.style.display==='inline-grid'){
+    r.style.display = 'none' ;
+  }
+  var q= document.getElementById('menu4');
+  if(q.style.visibility==='visible'){
+    q.style.visibility = 'hidden';
+  }
+  var p =document.getElementById('con1');
+  if(p.style.display==='inline-grid'){
+    p.style.display = 'none';
+  }
+  var o =document.getElementById('menu5');
+  if(o.style.visibility==='visible'){
+    o.style.visibility = 'hidden' ;
+  }
+
+}
+
+function AbreElementos(){
+  var x = document.getElementById('elem1');
+  if(x.style.display==='none'){
+    x.style.display = 'inline-grid';
+    x.style.placeitems = 'center' ;
+    x.style.marginleft = 'auto';
+    x.style.marginright = 'auto' ;
+  } else{
+    x.style.display = 'none';
+  }
+  var y = document.getElementById('menu1');
+  if(y.style.visibility==='hidden'){
+    y.style.visibility = 'visible';
+  } else {
+    y.style.visibility = 'hidden';
+  }
+}
+
+
+function AbreAnalisis(){
+  var x = document.getElementById('ana1');
+  if(x.style.display==='none'){
+    x.style.display = 'inline-grid';
+    x.style.placeitems = 'center' ;
+    x.style.marginleft = 'auto';
+    x.style.marginright = 'auto' ;
+  } else{
+    x.style.display = 'none';
+  }
+  var y = document.getElementById('menu2');
+  if(y.style.visibility==='hidden'){
+    y.style.visibility = 'visible';
+  } else {
+    y.style.visibility = 'hidden';
+  }
+}
+
+function AbrePenumbra(){
+  var x = document.getElementById('pen1');
+  if(x.style.display==='none'){
+    x.style.display = 'inline-grid';
+    x.style.placeitems = 'center' ;
+    x.style.marginleft = 'auto';
+    x.style.marginright = 'auto' ;
+  } else{
+    x.style.display = 'none';
+  }
+  var y = document.getElementById('menu3');
+  if(y.style.visibility==='hidden'){
+    y.style.visibility = 'visible';
+  } else {
+    y.style.visibility = 'hidden';
+  }
+}
+
+
+function AbreLuz(){
+  var x = document.getElementById('luz1');
+  if(x.style.display==='none'){
+    x.style.display = 'inline-grid';
+    x.style.placeitems = 'center' ;
+    x.style.marginleft = 'auto';
+    x.style.marginright = 'auto' ;
+  } else{
+    x.style.display = 'none';
+  }
+  var y = document.getElementById('menu4');
+  if(y.style.visibility==='hidden'){
+    y.style.visibility = 'visible';
+  } else {
+    y.style.visibility = 'hidden';
+  }
+}
+
+
+function AbreConclusion(){
+  var x = document.getElementById('con1');
+  if(x.style.display==='none'){
+    x.style.display = 'inline-grid';
+    x.style.placeitems = 'center' ;
+    x.style.marginleft = 'auto';
+    x.style.marginright = 'auto' ;
+  } else{
+    x.style.display = 'none';
+  }
+  var y = document.getElementById('menu5');
+  if(y.style.visibility==='hidden'){
+    y.style.visibility = 'visible';
+  } else {
+    y.style.visibility = 'hidden';
+  }
+}
